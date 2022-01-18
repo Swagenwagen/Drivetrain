7b792347e4eb6f059ad23aad3a483ea9fa208c0a
Swagenwagen <49845822+Swagenwagen@users.noreply.github.com>
2022-01-18 13:26:41 -0700

    Added drivetrain files

---
+++ 2020-Robot-main-Imported/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,58 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants.  This class should not be used for any other purpose.  All constants should be
+ * declared globally (i.e. public static).  Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+	public static final double drivekD = 0;
+	public static final double drivekI = 0;
+	public static final double drivekP = 0;
+	
+	// CAN IDs
+    public int 	shooter  = 1;
+    public int 	FrontRight = 0;
+    public int FrontLeft = 20;
+    public int 	BackRight = 2;
+    public int BackLeft = 3;
+    public int Turret = 7;
+    public int intake = 8;
+    public int serializer2 = 15;
+    public int serializer1 = 9;
+    public int indexer = 10;  
+    
+    // Photoe DIO IDs
+    public int photoe1 = 0;
+    public int photoe2 = 1;
+
+    // Shooter PID Configurations
+    public double shooterkP = 0.009;
+    public double shooterkF = 0.001;
+    public double shooterkI = 0.000095;
+    public double shooterkD = 0.0045;
+    public int shooterIntZone = 540;
+
+
+    // TODO: tune turret pid values to not oscillate when the turret turns to an object
+    // Turret PID Configurations
+
+    public static double turretkP = .0015;
+    public static double turretkI = 0.01;
+    public static double turretkD = 0.0;
+    public static int turretIntZone = 400;
+    //Initial Pose
+    public double InitialX = 0;
+    public double InitialY = 0;
+
+}
---
+++ 2020-Robot-main-Imported/src/main/java/frc/robot/subsystems/Vision.java
@@ -0,0 +1,141 @@
+// /*----------------------------------------------------------------------------*/
+// /* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+// /* Open Source Software - may be modified and shared by FRC teams. The code   */
+// /* must be accompanied by the FIRST BSD license file in the root directory of */
+// /* the project.                                                               */
+// /*----------------------------------------------------------------------------*/
+
+// package frc.robot.subsystems;
+
+// import edu.wpi.first.wpilibj2.command.SubsystemBase;
+// import frc.robot.Constants;
+// import frc.robot.Robot;
+// import edu.wpi.first.networktables.NetworkTableEntry;
+// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+// import edu.wpi.first.networktables.NetworkTable;
+// import edu.wpi.first.networktables.NetworkTableInstance;
+
+// public class Vision extends SubsystemBase {
+
+//     // Instantiate Constants class
+//     Constants consts = new Constants();
+
+//     // PixyCam
+//     // Create instance for PixyCam
+//     // private Pixy2 wof;
+//     // Check PixyCam state
+//     // private int state;
+//     // Store PixyCam Info
+//     // ArrayList <Block> blocks;
+//     // Array Color
+//     // final String[] colors = {"", "Blue", "Green", "Red", "Yellow"}; 
+//     // Existence of camera
+//     // private boolean isCamera;
+
+//     // // Photoelectric Sensor
+//     // private DigitalInput frontBottomPh;
+//     // private DigitalInput backBottomPh;
+//     // Var for count # of balls in robot
+//     // private int count;
+
+//     //lime light
+//     NetworkTable limelight = NetworkTableInstance.getDefault().getTable("limelight");
+//     NetworkTableEntry tx = limelight.getEntry("tx");
+//     int targetHeight = 98;
+//     int limeLightHeight = 35;
+//     int limeLightAngle = 60;
+//     double limeLightAngleToTarget;
+
+//     public void initDefaultCommand() {
+//     }
+
+//     public Vision() {
+//         // PixyCam var initialize
+//         // isCamera = false;
+//         //wof = Pixy2.createInstance(LinkType.SPI);
+//         // state = -1;
+
+//         // Photoelectric var initialize
+//         // frontBottomPh = new DigitalInput(consts.photoe1);
+//         // backBottomPh = new DigitalInput(consts.photoe2);
+
+//     }
+
+//     @Override
+//     public void periodic() {
+//         // This method will be called once per scheduler run
+//     }
+//     public void GetDistance() {
+//         limeLightAngleToTarget = Robot.ts.getDouble(0.0);
+//         double distance = (targetHeight - limeLightHeight) / Math.tan(limeLightAngle + limeLightAngleToTarget);
+//         SmartDashboard.putNumber("distance", distance);
+//     }
+//     public double GetX() {
+//         return 0.0;
+//         // SmartDashboard.putNumber("XLimelight: ", Robot.tx.getDouble(0));
+//         // return Robot.tx.getDouble(0);
+//     }
+
+//     // public void countBalls() {
+//     //     SmartDashboard.putBoolean("front bottom state", frontBottomPh.get());
+//     //     SmartDashboard.putBoolean("back bottom state", backBottomPh.get());
+//     // }
+
+//     // Read colors on WOF
+//     // public void readColor() {
+//     //     if(!isCamera){
+//     //         // Initialize PixyCam if no camera present
+//     //         state = wof.init(0);
+//     //     }
+//     //     isCamera = state >= 0;
+
+//     //     // run getBlocks with arguments to have the camera
+//     //     // 15 represents reading 4 signatures
+//     //     // 255 represents read max number of blocks 
+//     //     wof.getCCC().getBlocks(false, 15, 255);
+
+//     //     // get blocks
+//     //     blocks = wof.getCCC().getBlocks();
+
+//     //     if(blocks.size() > 0) {
+//     //         // x and y position of largest target
+//     //         double xCoord = blocks.get(0).getX();
+//     //         double yCoord = blocks.get(0).getY();
+
+//     //         int signature = blocks.get(0).getSignature();
+
+//     //         // String containing target info
+//     //         String data = blocks.get(0).toString();
+
+
+//     //         // Put to dashboard
+//     //         SmartDashboard.putBoolean("Present: ", true);
+//     //         SmartDashboard.putNumber("X Coordinate: ", xCoord);
+//     //         SmartDashboard.putNumber("Y Coordinate: ", yCoord);
+//     //         SmartDashboard.putString("Color: ", colors[signature]);
+//     //         SmartDashboard.putString("Data: ", data);
+//     //     }
+//     //     else {
+//     //         SmartDashboard.putBoolean("Present: ", false);
+//     //         // Check to see how many targets are present
+//     //         SmartDashboard.putNumber("Size: ", blocks.size());
+//     //     }
+//     // }
+    
+//     // Calls Photoe sensor to count number of balls intaked
+//     // boolean prevPhFront = frontBottomPh.get();
+//     // boolean prevPhBack = backBottomPh.get();
+//     // public void count() {
+//     //     if(backBottomPh.get() == true && prevPhFront) {
+//     //         count++;
+//     //     }
+        
+//     //     if(frontBottomPh.get() == true && prevPhBack) {
+//     //         count--;
+//     //     }
+//     //     SmartDashboard.putNumber("# of balls: ", count);
+        
+//     //     prevPhFront = frontBottomPh.get();
+//     //     prevPhBack = backBottomPh.get();
+//     // }
+// }
